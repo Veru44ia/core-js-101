eb6853cd0b42a7d6c5c7ae52a3b60fe7ebf2e00d
Veru44ia <vera3188@gmail.com>
2022-01-13 02:34:38 +0300

    feat: add solutions 04-date-tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -79,8 +79,8 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  return new Date(endDate - startDate).toISOString().slice(11, -1);
 }
 
 
@@ -100,8 +100,14 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hours = date.getUTCHours() - 12;
+  const minutes = date.getUTCMinutes();
+  const angle = Math.abs((60 * hours - 11 * minutes) / 2);
+  if (angle > 180) {
+    return ((360 - angle) * Math.PI) / 180;
+  }
+  return (angle * Math.PI) / 180;
 }
 
 
